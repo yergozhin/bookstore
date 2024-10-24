833776bc819a3e0ca3e05ba370871b19ebe5c491
s28672 <s28672@pjwstk.edu.pl>
2024-10-24 22:53:47 +0200

    Update UnitTest1.cs

--- bookstoreTest/UnitTest1.cs
+++ bookstoreTest/UnitTest1.cs
@@ -10,6 +10,7 @@ namespace Bookstore.@class.Tests
     public class Tests
     {
         private readonly User user = new User("John Doe", "123456789", "john.doe@example.com", new DateTime(1990, 1, 1));
+        private readonly User user2 = new User("Jane Doe", "987654321", "jane.doe@example.com", new DateTime(1995, 5, 5));
 
         [Test]
         public void TestUserGetInformation()
@@ -19,6 +20,24 @@ namespace Bookstore.@class.Tests
             Assert.That(user.Email, Is.EqualTo("john.doe@example.com"));
             Assert.That(user.DateOfBirth, Is.EqualTo(new DateTime(1990, 1, 1)));
         }
+        [Test]
+        public void TestUserClassExtentStoresCorrectInstances()
+        { 
+            List<User> users = User.GetUsers();
+
+            Assert.That(users.Count, Is.EqualTo(2));
+            Assert.That(users[0].Name, Is.EqualTo("John Doe"));
+            Assert.That(users[1].Name, Is.EqualTo("Jane Doe"));
+        }
+        [Test]
+        public void TestUserEncapsulation()
+        {
+            user.Name = "John Smith";
+            List<User> users = User.GetUsers();
+
+            Assert.That(users[0].Name, Is.EqualTo("John Smith"));
+        }
 
     }
 }
+    
