4ca809c69fdc803eb9e3d484d5d2d9d628b25f49
Angelo Junior Irakoze <s21391@pjwstk.edu.pl>
2024-11-07 18:41:46 +0100

    UnitTest TestGetUser zqs deleted since there is another test that does the same

--- bookstoreTest/UnitTest1.cs
+++ bookstoreTest/UnitTest1.cs
@@ -12,6 +12,7 @@ namespace Bookstore.@class.Tests
         private readonly User user = new User("John Doe", "123456789", "john.doe@example.com", new DateTime(1990, 1, 1));
         private readonly User user2 = new User("Jane Doe", "987654321", "jane.doe@example.com", new DateTime(1995, 5, 5));
 
+        private readonly Book book = new Book("Dragon ball, the fury", 150, "English", null, "English");
         [Test]
         public void TestUserClassExtentStoresCorrectInstances()
         { 
@@ -39,19 +40,7 @@ namespace Bookstore.@class.Tests
         }
 
         /*
-         * Test commented out because it doesn't execute
-        [Test]
-        public void TestGetUser()
-        {
-            var users = user.getUsers();
-            users.Add(user2);            
-            Assert.That(user2.PhoneNumber.Equals(users[0].PhoneNumber));
-            
-        }
-        */
-
-        /*
-         * Commented out for the moment because it doesn't execute for now
+         * Commented out for the moment because it prints an error for now
         [Test]
         public void TestUserGetInformation()
         {
@@ -61,7 +50,6 @@ namespace Bookstore.@class.Tests
             Assert.That(user.DateOfBirth, Is.EqualTo(new DateTime(1990, 1, 1)));
         }
         */
-
     }
 }
     
